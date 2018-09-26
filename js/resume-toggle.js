3808892eb0f0995a9d2e13c0b5a7d2cf84834b05
halleyl <halleyl@uw.edu>
2018-09-26 12:50:01 -0700

    Add tooltip for style toggle button

--- js/resume-toggle.js
+++ js/resume-toggle.js
@@ -1,10 +1,33 @@
+// Set variables for toggle elements
 var toggleContainer = document.querySelector("#toggle")
 var toggleButton = document.querySelector("#toggle img")
 var toggleText = document.querySelector("#toggle p")
 var currentStyle = document.querySelector("link:nth-of-type(6)")
 var currentMainStyle = document.querySelector("link:nth-of-type(7)")
 
+// Empty var for currently chosen site style
 var styleChosen
+
+// Add tooltip for hover over toggle
+toggleContainer.addEventListener("mouseover", tooltip, false)
+toggleContainer.addEventListener("mouseout", tipOff, false)
+
+// Set variables for tooltip elements
+var ttBox = document.createElement("div")
+var ttText = document.createTextNode("Click the toggle to change the site's style!")
+
+// When the toggle is moused over, create & display the tooltip
+function tooltip() {
+  ttBox.setAttribute("id", "tooltip")
+  ttBox.appendChild(ttText)
+  toggleContainer.appendChild(ttBox)
+}
+
+// When the toggle is moused out, remove the tooltip
+function tipOff() {
+  toggleContainer.removeChild(ttBox)
+  ttBox.removeChild(ttText)
+}
 
 // Check to see if a stylesheet was already chosen by user
 if (localStorage.getItem("sheet") === "retro") {
